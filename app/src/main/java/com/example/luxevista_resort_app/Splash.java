9afd5236db9ef7bb75e934c576fed118a3ce2169
Aki-1407 <im.deepak1407@gmail.com>
2025-08-29 20:17:27 +0530

    Splash and welcome screen created and connected

--- app/src/main/java/com/example/luxevista_resort_app/Splash.java
+++ app/src/main/java/com/example/luxevista_resort_app/Splash.java
@@ -1,6 +1,9 @@
 package com.example.luxevista_resort_app;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +13,24 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class Splash extends AppCompatActivity {
 
+    private final int SPLASH_TIME_OUT = 3000;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_splash);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                // Intent to start Welcome_Screen Activity
+                // Assuming Welcome_Screen.class is the correct name of your target Activity
+                Intent mainIntent = new Intent(Splash.this, Welcome_Screen.class);
+                startActivity(mainIntent);
+
+                // Close Splash Activity so the user can't go back to it
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
     }
 }
